37e5af0a8b1a4362108690380b1a8d1792665deb
Abhinav Gauniyal <agauniyal@users.noreply.github.com>
2016-10-05 00:30:07 +0530

    Updated at hyperdev.com

--- server.js
+++ server.js
@@ -1,29 +1,24 @@
 'use strict';
 
-// require packages
-const express = require('express');
+// require pug and http server
 const pug = require('pug');
+const http = require('http');
+
+// create a new template for pug to parse
+const pugTemplate = "h1= name";
+
+// set locals
+const locals = {
+  name: "Abhinav Gauniyal"
+}
+
+// set compile Function for that template
+var compileFn = pug.compile(pugTemplate);
+// produce html output with compileFn on these locals
+var html = compileFn(locals);
 
 
-let locals = {
-  title: "Let's learn Pug",
-  heading: 'Welcome to Index page',
-  webUrl: 'https://pugjs.org/api/getting-started.html',
-  githubUrl: 'https://github.com/pugjs/pug'
-};
-
-// Let's create an app out of express
-const app = express();
-
-// Let's use pug
-app.set('view engine', 'pug');
-
-// some default routes - defined in routes.js
-app.get('/', function(request, response){
-  response.render('index', locals);
-});
-
-// listen for requests :)
-var listener = app.listen(process.env.PORT, function () {
-  console.log('Your app is listening on port ' + listener.address().port);
-});
+// create a new webserver and send template
+http.createServer((request, response) => {
+  response.end(html);
+}).listen(process.env.PORT);
